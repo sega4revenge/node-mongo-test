c7f5a64947431fea4ad0505262698f8f2e39c1d2
LINH <hohoanglinh190295@gmail.com>
2017-08-02 09:16:23 +0700

    gh

--- routes.js
+++ routes.js
@@ -100,7 +100,9 @@ module.exports = router => {
         form.uploadDir = uploadDir;
         form.parse(req, (err, fields, files) => {
             if (err) return res.status(500).json({ error: err });
-            console.log(files.path);
+            console.log({ message: fields });
+
+            // console.log(files);
             res.status(200).json({ uploaded: true , name : fields.name})
         });
         form.on('fileBegin', function (name, file) {
